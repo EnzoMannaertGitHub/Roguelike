f0944889865af50538c4ab604fed3dfff455c664
EnzoMannaertGitHub <mannaert.enzo@hotmail.be>
2021-07-05 12:55:41 +0200

    Coin pickup

---
+++ Game/Assets/Game/Item/Instant usables/Coin.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int _value = 1;
+    [SerializeField] LayerMask _layermask;
+    private Rigidbody2D _rigidBody;
+    private void Awake()
+    {
+        _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
