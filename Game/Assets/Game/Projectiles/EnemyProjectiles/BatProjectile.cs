bedacc77073b90a1e5225c7c37abbffca5f59a2f
EnzoMannaertGitHub <mannaert.enzo@hotmail.be>
2021-07-05 12:26:03 +0200

    Enemy projectile behavior

--- Game/Assets/Game/Projectiles/EnemyProjectiles/BatProjectile.cs
+++ Game/Assets/Game/Projectiles/EnemyProjectiles/BatProjectile.cs
@@ -7,4 +7,17 @@ public class BatProjectile : MonoBehaviour
     {
         gameObject.GetComponent<Rigidbody2D>().velocity = direction * _speed;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Health>().GetHit(5, gameObject.GetComponent<Rigidbody2D>().velocity);
+            Destroy(gameObject);
+        }
+        if (other.gameObject.layer != 7)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
