231d964e4a5ba67d959a3f42dc203293244fbfbf
EnzoMannaertGitHub <mannaert.enzo@hotmail.be>
2021-07-08 20:21:25 +0200

    text above itemstand

--- Game/Assets/Game/Shop/ItemStand/itemStand.cs
+++ Game/Assets/Game/Shop/ItemStand/itemStand.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using System.Collections.Generic;
 
 public class itemStand : MonoBehaviour
 {
+    [SerializeField] Text _costText;
+
     private int _cost = 1;
     private bool _isActive = true;
     [SerializeField] private List<GameObject> _items;
@@ -12,6 +13,11 @@ public class itemStand : MonoBehaviour
     private GameObject _item;
     private void Start()
     {
+        if (_cost == 0)
+            _costText.text = "";
+        else
+            _costText.text = $"{_cost}";
+
         int index = Random.Range(0, _items.Count);
         _item = Instantiate(_items[index], _itemLocation.position, _itemLocation.rotation);
     }
