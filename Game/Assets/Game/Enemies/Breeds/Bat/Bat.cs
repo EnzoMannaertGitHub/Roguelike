60758de689b1f1900f0cb5046abd62d34fa20d19
EnzoMannaertGitHub <mannaert.enzo@hotmail.be>
2021-07-10 13:31:17 +0200

    bat behavior adjusted

--- Game/Assets/Game/Enemies/Breeds/Bat/Bat.cs
+++ Game/Assets/Game/Enemies/Breeds/Bat/Bat.cs
@@ -56,6 +56,9 @@ public class Bat : Breed
 
     public override void OnPlayerHit(GameObject g)
     {
+        if (_elevating)
+            return;
+
         if (g.CompareTag("Player"))
         {         
             Vector2 dir = new Vector2();
