b26f66f2c6d705625f7d654cee457935b61a16bb
EnzoMannaertGitHub <mannaert.enzo@hotmail.be>
2021-08-25 11:52:44 +0200

    Shop adjustments

--- Game/Assets/Game/Shop/Portal/portal.cs
+++ Game/Assets/Game/Shop/Portal/portal.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 public class portal : MonoBehaviour
 {
     private GameObject _player;
-    private Vector3 _spawn;
+    [SerializeField] private Transform _spawn;
     [SerializeField] private bool _returnPortal = false;
     [SerializeField] private EnemySpawner _enemySpawner;
+    [SerializeField] private Shop _shop;
     private void Start()
     {
         _player = FindObjectOfType<PlayerMovement>().gameObject;
@@ -14,15 +15,14 @@ public class portal : MonoBehaviour
         if (!collision.CompareTag("Player") || collision.gameObject.layer != 0)
             return;
 
-        _spawn = new Vector3(14, 9, 0);
-
         if (_returnPortal)
         {
-            _spawn = new Vector3(3, 2, 0);
             _enemySpawner.ClearEnemies();
-            _spawn = new Vector3(3, 1, 0);
             LevelManager.Instance.LoadNewLevel();
+            _shop.IsInShop = false;
+            return;
         }
-        _player.transform.position = _spawn;
+        _player.transform.position = _spawn.position;
+        _shop.IsInShop = true;
     }
 }
--- Game/Assets/Game/Shop/Shop.cs
+++ Game/Assets/Game/Shop/Shop.cs
@@ -2,13 +2,30 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Shop : MonoBehaviour
 {
-    [SerializeField] List<Transform> _spawnLocations;
-    [SerializeField] GameObject _itemStand;
+    [SerializeField] private List<Transform> _spawnLocations;
+    [SerializeField] private GameObject _itemStand;
+    [SerializeField] private GameObject _shopSpawn;
+    private GameObject _player;
     List<GameObject> _itemStands = new List<GameObject>();
+    private bool _isInShop = false;
+    public bool IsInShop { get { return _isInShop; } set { _isInShop = value; } }
     // Start is called before the first frame update
     private void Start()
     {
         InstantiateItemStands();
+
+        _player = FindObjectOfType<PlayerMovement>().gameObject;
+    }
+
+    private void Update()
+    {
+        if (_isInShop)
+        {
+            if (_player.transform.position.y < _shopSpawn.transform.position.y - 1.5f)
+            {
+                _player.transform.position = _shopSpawn.transform.position;
+            }
+        }
     }
 
     public void ReloadShop()
