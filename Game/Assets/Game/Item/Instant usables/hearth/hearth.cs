676757863de52fd638dad7c0089fa37f93665a2a
TomStevens7533 <34093176+TomStevens7533@users.noreply.github.com>
2021-09-16 13:40:47 +0200

    size adjusment hp refill pickup

--- Game/Assets/Game/Item/Instant usables/hearth/hearth.cs
+++ Game/Assets/Game/Item/Instant usables/hearth/hearth.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class hearth : MonoBehaviour
 {
+    private void Start()
+    {
+        Vector3 scale = (GetComponentInChildren<SpriteRenderer>().transform.localScale / 4f);
+        GetComponentInChildren<SpriteRenderer>().transform.localScale = scale;
+
+        Vector2 colliderPos = GetComponent<BoxCollider2D>().transform.position;
+        Vector2 colliderSize = GetComponent<BoxCollider2D>().bounds.extents;
+
+        //set sprite to collider Pos with extra height
+        Vector3 spritePos = GetComponentInChildren<SpriteRenderer>().transform.position;
+        GetComponentInChildren<SpriteRenderer>().transform.position = colliderPos - colliderSize;
+        GetComponentInChildren<SpriteRenderer>().transform.position += new Vector3 (0,spritePos.y / 3.5f, 0);
+    }
     [SerializeField] private LayerMask _layermask;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && collision.gameObject.layer != 8)
         {
             collision.GetComponent<Health>().ResetHealthToMax();
             Destroy(gameObject);
