0e50653aecf5e4dbaf1baf928f7c1f7643139415
EnzoMannaertGitHub <mannaert.enzo@hotmail.be>
2021-09-27 13:37:50 +0200

    Deathbox under level

--- Game/Assets/Game/DeathboxScript.cs
+++ Game/Assets/Game/DeathboxScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DeathboxScript : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject other = collision.gameObject;
         if (other.CompareTag("Player"))
